1bac57d901bb3032dd2823d25dea3841dff34e4d
小揚 黃 <s9161309@hotmail.com>
2021-01-04 22:58:19 +0800

    Add console app project

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using ClassLibrary;
+
+namespace ConsoleApp
+{
+    static class Program
+    {
+        private static void Main()
+        {
+            System.Console.WriteLine($"1 + 1 = {Algo.Add(1, 1)}");
+        }
+    }
+}
